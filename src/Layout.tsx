949d8279f249fce6bb8d102498308cbad4452a96
Max Haak <haakm7@gmail.com>
2025-06-10 16:19:41 -0700

    fix modal not closing on redirect

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Outlet, useNavigation, useLocation } from 'react-router-dom';
 import TopBar from './components/TopBar';
 import MovieFormModal from './components/MovieForm/MovieFormModal';
 
 const Layout = () => {
-    const [isMovieFormModalOpen, setisMovieFormModalOpen] = useState(false)
+    const [isMovieFormModalOpen, setIsMovieFormModalOpen] = useState(false)
+    const navigation = useNavigation();
+    const location = useLocation();
+  
+    // Close the modal when the navigation finishes and we're no longer on the page where the modal was opened
+    useEffect(() => {
+      if (navigation.state === 'idle') {
+        setIsMovieFormModalOpen(false);
+      }
+    }, [navigation.state, location]);
 
     return (
         <div className='flex flex-col min-h-screen bg-background'>
-            <TopBar handleCreateShowtimeClick={() => setisMovieFormModalOpen(true)}/>
+            <TopBar handleCreateShowtimeClick={() => setIsMovieFormModalOpen(true)}/>
             <main className='flex-grow w-full'>
                 <Outlet />
             </main>
-            <MovieFormModal isOpen={isMovieFormModalOpen} onClose={() => setisMovieFormModalOpen(false)} />
+            <MovieFormModal isOpen={isMovieFormModalOpen} onClose={() => setIsMovieFormModalOpen(false)} />
         </div>
     )
 }
