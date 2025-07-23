98884553427c18041abe8d6808b7904f2b2a0770
Max Haak <haakm7@gmail.com>
2025-07-23 11:40:46 -0700

    remove duplicated frontend files

--- src/components/Calendar/ShowtimePeriod.tsx
+++
@@ -1,8 +0,0 @@
-const ShowtimePeriod = ({ tooltip }: { tooltip?: string }) => (
-    <div
-      className={`absolute top-0 left-0 w-full h-full rounded-sm pointer-events-none opacity-70 bg-primary`}
-      title={tooltip}
-    />
-)
-
-export default ShowtimePeriod
--- src/constants/layout.ts
+++
@@ -1,2 +0,0 @@
-// layout.ts
-export const CELL_HEIGHT_CLASSES = "h-4 sm:h-4 md:h-5 lg:h-6 xl:h-7";
