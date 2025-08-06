7672bba812ec989af4f9e693a74598c76602425b
Max Haak <haakm7@gmail.com>
2025-08-06 14:30:53 -0700

    add format filters to showtimes list

--- frontend/src/components/ShowtimeList.tsx
+++ frontend/src/components/ShowtimeList.tsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, {useState} from "react"
 import type { Showtime } from "../types/showtime"
 import { format } from 'date-fns'
 import { toZonedTime } from 'date-fns-tz'
+import { Checkbox } from "@headlessui/react"
 
 
 type ShowtimeListProps  = {
@@ -11,11 +12,21 @@ type ShowtimeListProps  = {
 }
 
 const ShowtimeList: React.FC<ShowtimeListProps> = ({showtimes, selectedUsers, eventTimezone}) => {
+    const [selectedFormats, setSelectedFormats] = useState<string[]>([])
+
     if (showtimes.length == 0) {
         return (
             <div className="flex items-center justify-center">
                 <h1 className="text-3xl text-center text-primary-shift-red md:text-4xl">Add user availabilities to see optimal showtimes.</h1>
             </div>
+        )
+    }
+
+    const handleFormatSelectionChange = (selectedFormat: string, checked: boolean) => {
+        setSelectedFormats(prev => 
+            checked 
+                ? [...prev, selectedFormat]
+                : prev.filter(format => format !== selectedFormat)
         )
     }
     
@@ -28,6 +39,7 @@ const ShowtimeList: React.FC<ShowtimeListProps> = ({showtimes, selectedUsers, ev
     }
 
     
+    const formats:string[] = []
 
     // Calculate percentages and sort in one chain
     const sortedShowtimes = showtimes
@@ -37,26 +49,52 @@ const ShowtimeList: React.FC<ShowtimeListProps> = ({showtimes, selectedUsers, ev
                 const filteredAvailableUsers = showtime.available_users.filter(userID => selectedUsers.includes(userID));
                 percentAvailable = Math.round((filteredAvailableUsers.length / selectedUsers.length) * 100);
             }
+            const format = showtime.showing_type
+            if (!formats.includes(format)) {
+                formats.push(format)
+            }
             return { ...showtime, percentAvailable };
+        })
+        .filter(showtime => {
+            // If no formats are selected, show all showtimes
+            if (selectedFormats.length === 0) return true;
+            // Otherwise, only show showtimes that match selected formats
+            return selectedFormats.includes(showtime.showing_type);
         })
         .sort((a, b) => b.percentAvailable - a.percentAvailable); // Sort descending by availability
 
     return (
-        <div className='flex flex-col items-start justify-around h-full overflow-y-auto max-h-[calc(100vh-20rem)] scrollbar-hidden gap-2 '>
-            {sortedShowtimes.map(showtime => {
-                return (
-                    <div className="flex-col w-full p-2 rounded-md bg-surface hover:bg-surface/50">
-                        <div className="flex justify-between gap-2">
-                            <h1 className="text-2xl text-text-primary">{format(toZonedTime(new Date(showtime.start_time), eventTimezone), "MMMM d - h:mmaaa")}</h1>
-                            <h1 className={`text-2xl ${getAvailabilityShade(showtime.percentAvailable)}`}>{showtime.percentAvailable + "% available"}</h1>
+        <div className="flex flex-col gap-5">
+            <div className='flex justify-around'>
+                {formats.map(format => {
+                    return (
+                    <Checkbox 
+                        checked={selectedFormats.includes(format)}
+                        onChange={(isChecked) => handleFormatSelectionChange(format, isChecked)}
+                        className='flex items-center gap-1 px-2 py-1 text-sm border rounded-md sm:text-base text-text-secondary border-text-secondary hover:bg-surface hover:cursor-pointer data-checked:text-primary data-checked:border-primary'
+                    >
+                        {format}
+                    </ Checkbox>
+                    )
+                })}
+            </div>
+            <div className='flex flex-col items-start justify-around h-full overflow-y-auto max-h-[calc(100vh-20rem)] scrollbar-hidden gap-2 '>
+                {sortedShowtimes.map(showtime => {
+                    return (
+                        <div className="flex-col w-full p-2 rounded-md bg-surface hover:bg-surface/50">
+                            <div className="flex justify-between gap-2">
+                                <h1 className="text-xl md:text-2xl text-text-primary">{format(toZonedTime(new Date(showtime.start_time), eventTimezone), "MMMM d - h:mmaaa")}</h1>
+                                <h1 className={`text-xl md:text-2xl ${getAvailabilityShade(showtime.percentAvailable)}`}>{showtime.percentAvailable + "% available"}</h1>
+                            </div>
+                            <h1 className="text-xl text-text-primary">{showtime.theater_name}</h1>
+                            <h4 className="text-text-secondary">{showtime.theater_address}</h4>
+                            <h4 className="text-text-secondary">{"Format: " + showtime.showing_type}</h4>
                         </div>
-                        <h1 className="text-xl text-text-primary">{showtime.theater_name}</h1>
-                        <h4 className="text-text-secondary">{showtime.theater_address}</h4>
-                    </div>
-                )
-            })}
-        </div>
-    )
+                    )
+                })}
+            </div>
+        </div>  
+    )    
 }
 
 export default ShowtimeList
